791dacede4c231ec5a11a6bf19b1add6c9ce3a56
Yoann <thebaultyoann@gmail.com>
2023-04-03 08:23:56 +0200

    return maj

--- app/routes.py
+++ app/routes.py
@@ -15,7 +15,7 @@ def esp32_info(arceau_id):
     arceau = Arceau.query.get(arceau_id)
     arceau.state = newState
     arceau.battery = newBattery
-    return 
+    return "OK"
 
 @app.route('/esp32/<int:arceau_id>/action', methods=['GET',])
 def esp32_info2(arceau_id):
